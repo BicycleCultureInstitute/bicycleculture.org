1e34e4d302eb46306ed29134baf118f90e5c6888
Christopher Lovejoy <c@lovejoy.io>
2014-05-19 12:56:40 -0700

    move footer action into <footer> tag

--- public/content/themes/bci/footer.php
+++ public/content/themes/bci/footer.php
@@ -1,9 +1,10 @@
 
-
-<?php wp_footer(); ?>
 <footer>
   &copy; 2013-<?php echo date('Y'); ?> Bicycle Culture Institute, 501(c)3
   <?php wp_nav_menu(array('theme_location' => 'contact')); ?>
+  <div class="built-in">
+    <?php wp_footer(); ?>
+  </div>
 </footer>
 </body>
 </html>
